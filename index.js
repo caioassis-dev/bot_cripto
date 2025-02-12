1720e948c60cabc801863bc8462c80a1f481d1c4
Caio Freire Lima De Assis <caio.assis@mundiale.com.br>
2025-02-11 21:21:05 -0300

    adicionando primeiras linhas de codigo para testar funcao axios.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const axios = require("axios");
+
+const buy_price = 96884;
+const sell_price = 97824;
+const symbol = "BTCUSDT";
+
+async function start() {
+    console.log("test")
+}
+
+// start onde vai começar e o 3000 é milisegundos que é 3 segundos.
+setInterval(start, 3000);
+
+start();
