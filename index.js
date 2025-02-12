f96f5e7fdbdc3f93203132d5513bd4d19472785b
Caio Freire Lima De Assis <caio.assis@mundiale.com.br>
2025-02-11 21:41:14 -0300

    add the first function with logic and request to buy and sell coins.

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 
 const axios = require("axios");
 
-const buy_price = 96884;
-const sell_price = 97824;
+const buy_price = 96003;
+const sell_price = 96380;
 const symbol = "BTCUSDT";
 
+const api_url = "https://testnet.binance.vision";
+
+let position = false;
+
+//open, high, low, close
+// it its the candle default.
+
 async function start() {
-    console.log("test")
+    const { data } = await axios.get(api_url + "/api/v3/klines?limit=21&interval=15m&symbol="+ symbol)
+    const lastCandle = data[data.length -1]
+    const price = parseFloat(lastCandle[4]);
+
+
+    console.clear();
+    console.log(`Price: ${price}`)
+
+    if(price <= buy_price && position === false){
+        console.log("buy")
+        position = true;
+    }else if(price >= sell_price && position === true){
+        console.log("sell")
+        position = false;
+    }else{
+        console.log("wait")
+    }
 }
 
-// start onde vai começar e o 3000 é milisegundos que é 3 segundos.
+
+
+// start it is the begin and the 3000 é miliseconds it is 3 seconds.
 setInterval(start, 3000);
 
 start();
